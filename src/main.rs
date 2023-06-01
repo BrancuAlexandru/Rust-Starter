aab162a4b0c5cc958b58370b2a2d2e5d45568350
BrancuAlexandru <alexandrubrancu@yahoo.com>
2023-06-01 04:11:40 +0300

    Changed to word guess terminal i/o game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-  println!("Hello World!");
+  println!("Guess the foood!");
+
+  println!("Please input your guess: ");
+
+  let mut guess = String::new();
+
+  io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line.");
+
+  let food = "pizza";
+
+  if guess.contains(food) {
+    println!("Your guess '{guess}' is correct!");
+  } else {
+    println!("Your guess was wrong!");
+  }
 }
